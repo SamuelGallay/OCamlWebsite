e296b91b5a816f8ef0b887f7edba8eb76caecae6
Samuel Gallay <samuel.gallay@gmail.com>
2021-04-24 21:30:20 +0200

    DOM Manipulation

--- client/client.ml
+++ client/client.ml
@@ -1,1 +1,10 @@
-print_endline "Hello from OCaml ! 4"
+open Webapi.Dom
+
+let () =
+  print_endline "Hello from OCaml ! 4";
+  match document |> Document.querySelector("body") with
+  | None -> ()
+  | Some body ->
+    let p = document |> Document.createElement("p") in
+    Element.setInnerText p "Helloo";
+    Element.appendChild p body
